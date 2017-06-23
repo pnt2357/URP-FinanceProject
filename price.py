62b931ac94af97dbc85a8af57b4dd46e0ba2ed7d
pnt2357 <pnt2357@naver.com>
2017-06-23 17:21:50 +0900

    Update price.py

--- price.py
+++ price.py
@@ -9,10 +9,10 @@ Data_source = 'google'
 start_date ="2015/01/01"
 end_date ="2015/05/01"
 
-price = data.DataReader(stock_code, data_source = Data_source, start = start_date, end= end_date)
+price = data.DataReader(stock_code, data_source = Data_source, start = start_date, end = end_date)
 price.tail()
 
-price[['Close']].plot(color='blue', title= stock_code)
+price[['Close']].plot(color ='blue', title = stock_code)
 
 
 
