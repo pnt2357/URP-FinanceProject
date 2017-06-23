d351081e7e30022b7963dbb565feeea3c09ae591
pnt2357 <pnt2357@naver.com>
2017-06-23 17:02:40 +0900

    Add files via upload

---
+++ price.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import numpy as np
+import pandas as pd
+from pandas_datareader import data, wb
+
+
+stock_code = 'AMZN'
+Data_source = 'google'
+start_date ="2015/01/01"
+end_date ="2015/05/01"
+
+price = data.DataReader(stock_code, data_source = Data_source, start = start_date, end= end_date)
+price.tail()
+
+price[['Close']].plot(color='blue', title= stock_code)
+
+
+
+# BIDU: baidu
+# AMZN: Amazon
+# FB: Facebook
+# TWTR: Twitter
+# MSFT: Microsoft
